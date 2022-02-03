ae9bf9ab4d7a4c425565cabfc2498894302aae14
ahmed-humayun <93235542+ahmed-humayun@users.noreply.github.com>
2022-02-02 22:59:21 -0500

    added functional login and logout page

--- socialClone/settings.py
+++ socialClone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -121,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = Path(BASE_DIR) / 'static'
 
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
